ce244b84910b2bdfb9c180455d2023f2721f9aa1
Alyaksei Shemic <alxdev@mail.ru>
2016-03-09 13:41:15 +0300

    check mistakes

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 mkdir /home/box/web/public
 mkdir /home/box/web/public/img
 mkdir /home/box/web/public/css
-mkdir /home/box/web/pulic/js
+mkdir /home/box/web/public/js
 mkdir /home/box/web/uploads
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-#sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 #sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
