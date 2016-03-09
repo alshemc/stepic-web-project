db5a837dc3ab38c7050d0dbc6efdb56939d0f965
root <root@bi-VirtualBox.(none)>
2016-03-10 02:47:17 +0300

    new task

---
+++ hello.py
@@ -0,0 +1,10 @@
+def wsgi_application(environ, start_response):
+	#business logic
+	status = '200 OK'
+	headers = [
+		('Content type', 'text/plain')
+	]
+	resp = environ['QUERY_STRING'].split("&")
+  	resp = [item+"\r\n" for item in resp]
+	start_response(status, headers)
+	return resp
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+
 mkdir /home/box/web/public
 mkdir /home/box/web/public/img
 mkdir /home/box/web/public/css
@@ -5,5 +6,7 @@ mkdir /home/box/web/public/js
 mkdir /home/box/web/uploads
 #sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-#sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
+#gunicorn -b 0.0.0.0:8080 hello:app
