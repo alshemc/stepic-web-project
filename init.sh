35ab9af6e3ec2a7f654dbd4685cdea997a6f0415
Alyaksei Shemic <alxdev@mail.ru>
2016-03-09 12:31:08 +0300

    add creates catalog

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+mkdir /home/box/web/public
+mkdir /home/box/web/public/img
+mkdir /home/box/web/public/css
+mkdir /home/box/web/pulic/js
+mkdir /home/box/web/uploads
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 #sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 #sudo rm /etc/nginx/sites-enabled/default
