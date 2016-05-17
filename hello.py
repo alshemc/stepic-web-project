8dc876d78c0184119259ea03e2c1f21eead7a317
Alyaksei Shemic <alxdev@mail.ru>
2016-05-18 01:01:24 +0300

    origin

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def wsgi_application(environ, start_response):
 	#business logic
 	status = '200 OK'
 	headers = [
-		('Content type', 'text/plain')
+		('Content-type', 'text/plain')
 	]
 	resp = environ['QUERY_STRING'].split("&")
   	resp = [item+"\r\n" for item in resp]
