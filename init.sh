aba7ede6a70b34a22ff09da91744e70724fc0305
Alyaksei Shemic <alxdev@mail.ru>
2016-05-23 23:51:53 +0300

    origin

--- init.sh
+++ init.sh
@@ -4,9 +4,14 @@ mkdir /home/box/web/public/img
 mkdir /home/box/web/public/css
 mkdir /home/box/web/public/js
 mkdir /home/box/web/uploads
+mkdir /home/box/web/ask
+mkdir /home/box/web/ask/ask
+mkdir /home/box/web/ask/qa
+#sudo django-admin.py startproject ask
 #sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
 #gunicorn -b 0.0.0.0:8080 hello:app
